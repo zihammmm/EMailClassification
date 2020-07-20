6e31559c7c8599015c3332da63206b6ec21b9f68
GaoWeiCR7 <1968821935@qq.com>
2020-07-20 23:43:29 +0800

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,33 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.util.GenericOptionsParser;
 
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        Configuration configuration = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(configuration, args).getRemainingArgs();
+        if (otherArgs.length < 2) {
+            System.err.println("args length wrong\n");
+            System.exit(2);
+        }
+        Job job = Job.getInstance(configuration, "test");
+        job.setJarByClass(Main.class);
+        job.setMapperClass(mapreduce.EMailMapper.class);
+        job.setReducerClass(mapreduce.EMailReducer.class);
 
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
 
+        FileInputFormat.addInputPath(job, new Path(otherArgs[0]));
+        FileOutputFormat.setOutputPath(job, new Path(otherArgs[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
--- src/main/java/mapreduce/EMailMapper.java
+++ src/main/java/mapreduce/EMailMapper.java
@@ -4,22 +4,26 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+import org.apache.hadoop.io.DoubleWritable;
 import preprocess.TextTokenizer;
 
 import java.io.IOException;
 import java.util.List;
 
-public class EMailMapper extends Mapper<Object, Text, Text, IntWritable> {
+public class EMailMapper extends Mapper<Object, Text, Text, DoubleWritable> {
     private Text word = new Text();
-    private IntWritable count = new IntWritable(1);
+    private DoubleWritable count = new DoubleWritable(1);
 
     @Override
     protected void map(Object key, Text value, Context context) throws IOException, InterruptedException {
         FileSplit fileSplit = (FileSplit)context.getInputSplit();
         String fileName = fileSplit.getPath().toString();
+        fileName = fileName.substring(5);
         List<String> list = TextTokenizer.getInstance().tokenize(fileName);
+        word.set("!#"+fileName);
+        context.write(word,new DoubleWritable(list.size()));
         for (String string : list) {
-            word.set(string);
+            word.set(string+'#'+fileName);
             context.write(word, count);
         }
     }
